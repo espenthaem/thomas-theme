ca303ea6b6af482d7fc2a7bd82ee8af948ed45d2
espenthaem <e.tierolff@gmail.com>
2020-10-28 10:33:50 +0100

    nav bar adjustments, including text dropdown

--- includes/section-navigation.php
+++ includes/section-navigation.php
@@ -20,11 +20,74 @@
 			</li>
 
 			<li>
-			    <a class="nav-link" href="<?php echo get_site_url();?>/text/">Text</a>
+			    <a id="a-text" class="nav-link" href="#" onmouseover="showSubLinkText()" onmouseout="hideSublinkText()">Text</a>
+			    
+			    <div id="text-sub-link"  class="sub-link-div">
+
+			    	<?php
+
+					$post = get_page_by_title('Text');
+
+					$args = array(
+					    'post_type'      => 'page',
+					    'posts_per_page' => -1,
+					    'post_parent'    => $post->ID,
+					    'order'          => 'ASC',
+					    'orderby'        => 'menu_order'
+					 );
+
+					$parent = new WP_Query($args);
+
+					if ($parent->have_posts()) {
+						$counter = 0;  
+						while ($parent->have_posts()){
+							$parent->the_post(); 
+								?>
+								<a href= <?php the_permalink();?>>
+									<?php the_title();?> 
+				            	</a>
+							<?php
+						}
+					} ?>
+
+					<?php wp_reset_postdata(); ?>
+
+			    </div>
 			</li>
 
 			<li>
 			    <a class="nav-link" href="<?php echo get_site_url();?>/kontakt/">Kontakt</a>
+
+			    	<div id="kontakt-sub-link" class="sub-link-div">
+			    
+				    	<?php
+
+						$post = get_page_by_title('Kontakt');
+
+						$args = array(
+						    'post_type'      => 'page',
+						    'posts_per_page' => -1,
+						    'post_parent'    => $post->ID,
+						    'order'          => 'ASC',
+						    'orderby'        => 'menu_order'
+						 );
+
+						$parent = new WP_Query($args);
+
+						if ($parent->have_posts()) {
+							$counter = 0;  
+							while ($parent->have_posts()){
+								$parent->the_post(); 
+									?>
+									<a href= <?php the_permalink();?>>
+										<?php the_title();?> 
+					            	</a>
+								<?php
+							}
+						} ?>
+
+						<?php wp_reset_postdata(); ?>
+			    </div>
 			</li>
 
 		</ul>
@@ -65,11 +128,11 @@
 	    // Change display style
 	    if (display == 'block') {
 	  	    arbeiten_div.style.display = 'none'
-	  	    document.getElementById("a-arbeiten").style.opacity =  1.0
+	  	    document.getElementById("a-text").style.opacity =  1.0
 	    }
 	    else if (display == 'none') {
 	  	   arbeiten_div.style.display = 'block'
-	  	   document.getElementById("a-arbeiten").style.opacity =  .5
+	  	   document.getElementById("a-text").style.opacity =  .5
 	    }
 
 	}
@@ -89,5 +152,20 @@
 	  	    archiv_div.style.visibility = 'hidden'
 	  	    document.getElementById("a-archiv").style.opacity =  1
 	    }
+	}
+
+	function showSubLinkText() {
+		var sub_link_div = document.getElementById("text-sub-link")
+	    
+	    sub_link_div.style.display = 'block'
+	  	document.getElementById("a-text").style.opacity =  .5
+	}
+
+	function hideSublinkText() {
+		var sub_link_div = document.getElementById("text-sub-link")
+
+		sub_link_div.style.display = 'none'
+	  	document.getElementById("a-text").style.opacity =  1
+	    
 	}
 </script>
