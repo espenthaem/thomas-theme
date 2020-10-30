f31b38e5e70e1b5c534f0690e26b8acb7dc29cf3
espenthaem <e.tierolff@gmail.com>
2020-10-30 16:05:08 +0100

    arbeiten structure rework + texts navigation fix

--- includes/section-navigation.php
+++ includes/section-navigation.php
@@ -7,12 +7,12 @@
 	<div id="navigation-bar-div" class=" flex">
 		
 		<ul class="nav">
-			<li class="dropdown">
-		    	<a id="a-arbeiten" class="nav-link" onclick="showArbeiten()" href="#">Arbeiten</a>
+			<li>
+		    	<a id="a-arbeiten" class="nav-link" href="<?php echo get_site_url();?>/arbeiten/">Arbeiten</a>
 		  	</li>
 
 			<li>
-				<a id="a-archiv" class="nav-link" href="<?php echo get_site_url();?>/archiv/"Archiv>Archiv</a>
+				<a id="a-archiv" class="nav-link" href="<?php echo get_site_url();?>/archiv/">Archiv</a>
 			</li>
 
 			<li>
@@ -20,39 +20,41 @@
 			</li>
 
 			<li>
-			    <a id="a-text" class="nav-link" href="#" onmouseover="showSubLinkText()" onmouseout="hideSublinkText()">Text</a>
-			    
-			    <div id="text-sub-link"  class="sub-link-div">
+				<div onmouseover="showSubLinkText()" onmouseout="hideSublinkText()">
+				    <a id="a-text" class="nav-link" href="#">Text</a>
+				    
+				    <div id="text-sub-link"  class="sub-link-div">
 
-			    	<?php
+				    	<?php
 
-					$post = get_page_by_title('Text');
+						$post = get_page_by_title('Text');
 
-					$args = array(
-					    'post_type'      => 'page',
-					    'posts_per_page' => -1,
-					    'post_parent'    => $post->ID,
-					    'order'          => 'ASC',
-					    'orderby'        => 'menu_order'
-					 );
+						$args = array(
+						    'post_type'      => 'page',
+						    'posts_per_page' => -1,
+						    'post_parent'    => $post->ID,
+						    'order'          => 'ASC',
+						    'orderby'        => 'menu_order'
+						 );
 
-					$parent = new WP_Query($args);
+						$parent = new WP_Query($args);
 
-					if ($parent->have_posts()) {
-						$counter = 0;  
-						while ($parent->have_posts()){
-							$parent->the_post(); 
-								?>
-								<a href= <?php the_permalink();?>>
-									<?php the_title();?> 
-				            	</a>
-							<?php
-						}
-					} ?>
+						if ($parent->have_posts()) {
+							$counter = 0;  
+							while ($parent->have_posts()){
+								$parent->the_post(); 
+									?>
+									<a href= <?php the_permalink();?>>
+										<?php the_title();?> 
+					            	</a>
+								<?php
+							}
+						} ?>
 
-					<?php wp_reset_postdata(); ?>
+						<?php wp_reset_postdata(); ?>
 
-			    </div>
+				    </div>
+				</div>
 			</li>
 
 			<li>
@@ -94,31 +96,9 @@
 	
 	</div>
 
-	<?php get_template_part('includes/section','arbeitentiles');?>
-
 </div>
 
 <script>
-	function showArbeiten() {
-	    var arbeiten_div = document.getElementById("arbeiten-tiles-div")
-	    var display = window.getComputedStyle(arbeiten_div).display
-	    // Change display style
-	    if (display == 'block') {
-    	    arbeiten_div.style.display = 'none'
-
-	  	    document.getElementById("a-arbeiten").style.opacity =  1.0
-	    }
-	    else if (display == 'none') {
-	  	   arbeiten_div.style.display = 'block'
-
-	  	   arbeiten_div.style.webkitAnimationName = 'fadeIn'; 
-    	   arbeiten_div.style.webkitAnimationDuration = '2s';
-
-	  	   document.getElementById("a-arbeiten").style.opacity =  .5
-	    }
-
-	}
-
 	function showSubLinkText() {
 		var sub_link_div = document.getElementById("text-sub-link")
 	    
