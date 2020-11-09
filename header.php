79c3f570a0edea136a9082d0a5ddcec865526bde
espenthaem <e.tierolff@gmail.com>
2020-11-09 15:08:47 +0100

    font adjustment

--- header.php
+++ header.php
@@ -4,7 +4,7 @@
 	<meta charset="utf-8">
 	<title>Thomas Schulz</title>
 
-	<link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@200&display=swap" rel="stylesheet"> 
+	<link href="https://fonts.googleapis.com/css2?family=Open+Sans:wght@300&display=swap" rel="stylesheet"> 
 	
 	<?php wp_head();?>
 
